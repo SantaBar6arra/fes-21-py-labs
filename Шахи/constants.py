e3d4e3d8c05a8e47a24f031515ad24462f68ec3b
SantaBar6arra <santabar6arr@gmail.com>
2023-04-14 22:25:55 +0300

    +mod ivashkiv

---
+++ Шахи/constants.py
@@ -0,0 +1,21 @@
+from game import *
+WHITE = "white"
+BLACK = "black"
+uniDict = {
+    BLACK : {
+        Pawn : "♙", 
+        Rook : "♖", 
+        Knight : "♘", 
+        Bishop : "♗", 
+        King : "♔", 
+        Queen : "♕" 
+    }, 
+    WHITE : {
+        Pawn : "♟", 
+        Rook : "♜", 
+        Knight : "♞", 
+        Bishop : "♝", 
+        King : "♚", 
+        Queen : "♛" 
+    }
+}
