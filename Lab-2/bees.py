0ffa7cae0e93a8f0d691bd5cfa5fde7dc58fad80
Misha <62945368+MishaFi@users.noreply.github.com>
2023-05-05 23:40:31 +0300

    Add files via upload
    Lab-2

---
+++ Lab-2/bees.py
@@ -0,0 +1,51 @@
+import random
+from helper import ab_distance
+
+
+class Hive:
+
+    def __init__(self):
+        self.position = (0, 0)
+        self.food_bank = 0
+        self.employed_bees = []
+        self.inspector_bee = None
+
+
+class EmployedBee:
+
+    def __init__(self):
+        self.position = (0, 0)
+        self.max_food_amout = random.randint(1, 10)
+        self.food_amout = 0
+        self.target_food_source = None
+
+    def fly(self, position, food_source=None):
+
+        self.position = position
+        self.target_food_source = food_source
+        if food_source is not None:
+            print(f"Employed bee is now at {self.position}.")
+
+    def gather(self):
+        self.food_amout = min(self.max_food_amout, self.target_food_source.food_amount)
+        self.target_food_source.food_amount -= self.food_amout
+        self.target_food_source.taken_food += self.food_amout
+        self.target_food_source = None
+        print(f"Employed bee gathered {self.food_amout} food from {self.position}.")
+            
+    def upload_food(self, hive):
+        hive.food_bank += self.food_amout
+        self.food_amout = 0
+        print(f"Hive now has {hive.food_bank} food in the bank.")
+
+
+class InspectorBee:
+
+    def __init__(self):
+        pass
+
+    def check_honey(self, source, all_taken_food) -> float:
+
+        source_quality = random.random() * all_taken_food / ab_distance((0, 0), source.position)
+        print(f"Inspector bee checked honey quality in {source.position} and found {source_quality}.")
+        return source_quality
---
+++ Lab-2/main.py
@@ -0,0 +1,57 @@
+import numpy as np
+from bees import *
+from field import *
+from time import sleep
+
+
+def run(size=(100, 100), food_sources_count=5, employed_bees_count=5, min_food_amout=1, max_food_amout=10):
+    hive = Hive()
+    field = Field(100, 100)
+
+    food_sources_count = 5
+    total_food_amout = 0
+    for i in range(food_sources_count):
+        x, y = np.random.randint(0, field.width), np.random.randint(0, field.height)
+        field.food_sources.append(FoodSource((x, y), np.random.randint(min_food_amout, max_food_amout)))
+        total_food_amout += field.food_sources[-1].food_amount
+
+    print(f"Total food amout: {total_food_amout}")
+
+    employed_bees_count = 5
+    for i in range(employed_bees_count):
+        hive.employed_bees.append(EmployedBee())
+
+    hive.inspector_bee = InspectorBee()
+
+    while hive.food_bank < total_food_amout:
+        sleep(1)
+
+        food_source_index = 0
+        for bee in hive.employed_bees:
+            if field.food_sources[food_source_index].honey_quality > 0:
+                food_source = field.food_sources[food_source_index]
+                bee.fly(food_source.position, food_source)
+                food_source_index += 1
+            else:
+                food_source_index = 0
+
+        for bee in hive.employed_bees:
+            if bee.target_food_source is not None:
+                bee.gather()
+
+        for bee in hive.employed_bees:
+            bee.fly((0, 0))
+            bee.upload_food(hive)
+
+        for source in field.food_sources:
+            source.honey_quality = hive.inspector_bee.check_honey(source, source.taken_food)
+            source.taken_food = 0
+
+        field.food_sources.sort(key=lambda source: source.honey_quality, reverse=True)
+
+    print("Simulation ended.")
+    print(f"Hive has {hive.food_bank} food in the bank.")
+
+
+if __name__ == "__main__":
+    run((100, 100), 5, 5, 10, 50)
