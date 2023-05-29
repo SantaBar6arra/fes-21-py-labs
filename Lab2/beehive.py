dffc818bcd8fd4e677ac281bc24ad7bb4cff648a
s0nkker <118204723+s0nkker@users.noreply.github.com>
2023-05-29 16:13:43 +0100

    Update beehive.py

--- Lab2/beehive.py
+++ Lab2/beehive.py
@@ -1,15 +1,6 @@
 import random
 from workers import Workerbee, QualityInspectorBee
 
-class Config:   
-    FIELD_HEIGHT = 20   
-    FIELD_WIDTH = 20   
-    COLONY_SIZE = 5    
-    DIMENSION = 2      
-    EMPLOYED_AGENTS = 3
-    SOURCES = 10
-    MAX_FIELD_CAPACITY = 100
-
 class Cell:
     __pos_x: int
     __pos_y: int
